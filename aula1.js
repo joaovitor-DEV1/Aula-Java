7486e40fcbeffed983e479286949d1b7b980df73
joao <lucajovihome@gmail.com>
2026-02-09 16:23:16 -0300

    first commit

---
+++ aula1.js
@@ -0,0 +1,103 @@
+
+// codigo não precisa de ponto e virgulaa no final
+
+// Antigo, pode ser redeclarado e mudar de valor.
+//var idade = 10;
+//var cor = "azul";
+//console.çog(cor);
+
+// Mais moderno, pode mudar de valor, mas não pode ser redeclarado
+//let nome = "rayka";
+//let fruta = "banana"
+//fruta = "maça" // pode mudar o valor, mas não pode ser redeclarado
+//console.log(fruta)
+
+// Fixo, não pode mudar de valor
+//const cpf = 4323403792;
+//const pi = 3.14;
+//pi = 10;
+//console.log(pi)
+
+// Desafios 
+
+// DESAFIO 1
+
+let nome = "joao"
+let idade = 18
+
+console.log(nome)
+console.log(idade)
+
+
+//DESAFIO 2
+
+let comida_favorita = "marcao"
+comida_favorita = "macarrao"
+console.log(comida_favorita)
+
+const pais_nascimento  = "Brasil";
+//pais_nascimento = "chile";
+console.log(pais_nascimento)
+
+//DESAFIO 3
+
+var indentidade = "joao"
+var idadee = 23
+var cidade = "guarulhos"
+
+console.log(indentidade)
+console.log(idadee)
+console.log(cidade)
+
+//DESAFIO 4
+
+let curso = "DS"
+curso = "mecatronica"
+
+// DESAFIO 5
+const Ano_atual =2026
+//Ano_atual = 2015
+console.log(Ano_atual)
+
+// DESAFIO 6
+
+var name = "jao"
+var sobre_nome ="vitor"
+
+console.log(name,sobre_nome)
+
+// DESAFIO 7
+
+var usuario = "pedro"
+var email = "Vitor.gonçalves@gmail.com"
+var city = "São Paulo"
+
+console.log(usuario, email, city)
+
+// DESAFIO 8
+
+var cm = 18
+var tamanho = 1.70
+
+console.log(cm)
+console.log(tamanho)
+
+// DESAFIO 9
+
+var not1 = 7
+var not2 = 5
+var not3 = 8
+
+console.log(not1)
+console.log(not2)
+console.log(not3)
+
+// DESAFIO 10 
+
+var pc = 2000.00 
+var console = 4000.00
+var ps5_da_haru = 10.00
+
+console.log(pc)
+console.log(console)
+console.log(ps5_da_haru)
