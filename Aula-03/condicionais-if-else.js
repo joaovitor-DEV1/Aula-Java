89ef596ae8825a884fdd983547f3759106f42fac
João Vitor Ulisses Bacurau <lucajovihome@gmail.com>
2026-02-23 14:51:42 -0300

    Add files via upload

---
+++ Aula-03/condicionais-if-else.js
@@ -0,0 +1,155 @@
+console.log(" ")
+console.log("ATIVIDADES")
+console.log(" ")
+
+//Exemplo 01
+let num = 18
+if (num >= 0){
+    console.log("Positivo")
+}
+else {
+    console.log("Negativo")
+}
+
+//Exemplo 02
+let idadE = 18
+if (idadE >= 18){
+    console.log("Você é maior de idade!")
+}
+else {
+    console.log("Você é menor de idade")
+}
+
+//Exemplo 03
+let num1 = 7/2
+if(num1 === 0){
+    console.log("Par")
+}
+else {
+    console.log("Ímpar")
+}
+
+
+//Exemplo 04
+let notA = 7
+if(notA >= 6){
+    console.log("Aprovado! 👏")
+}
+else {
+    console.log("Reprovado! 😥")
+}
+
+
+
+//Exemplo 05
+let votar = 18
+if (votar >= 16){
+    console.log("pode votar")
+}
+else {
+    console.log("não pode votar")
+}
+
+//Exemplo 06
+let senhA = "1234"
+if (senhA === "1234"){
+    console.log("Acesso permitido!")
+}
+else { 
+    console.log("Senha incorreta")
+}
+
+//Exemplo 07
+let temp = 25
+if (temp >= 25){
+    console.log("Está quente")
+}
+else {
+    console.log("Está frio")
+}
+
+//Exemplo 08
+let altura = 1.70
+if (altura >= 1.50){
+    console.log("Pode entrar")
+}
+else {
+    console.log("Não pode entrar")
+}
+
+//Exemplo 09
+let num3 = 18
+if (num3 >= 1){
+    console.log("Positivo")
+}
+else if(num3 === 0){
+    console.log("igual a zero")
+}
+else {
+    console.log("Negativo")
+}
+
+//Exemplo 10
+let nota1 = 7
+if(nota1 >= 9){
+    console.log("Excelente!")
+}
+else if(nota1 === 7 || 8) {
+    console.log("Bom")
+}
+else if(nota1 === 6) {
+    console.log("Regular")
+}
+else {
+    console.log("Reprovado!")
+}
+
+//Exemplo 11
+let cnh = true
+let idade2 = 18
+if (idade2 >= 18 && cnh){
+    console.log("pode dirigir")
+}
+else {
+    console.log("não pode dirigir")
+}
+
+//Exemplo 12
+let compra = 101
+if (compra > 100){
+    console.log("ganhou desconto")
+}
+else {
+    console.log("sem desconto")
+}
+
+//Exemplo 13
+let usuario = "admin"
+let senh = "123"
+if (usuario === "admin" && senh === "123"){
+    console.log("login realizado")
+}
+else {
+    console.log("erro de login")
+}
+
+//Exemplo 14
+let idade3 = 18
+if (idade3 >= 16){
+    console.log("Pode assistir")
+}
+else {
+    console.log("Não pode assistir")
+}
+
+//Exemplo 15
+let cor1 = "Verde" && "Amarelo" && "Vermelho" 
+if (cor1 === "Verde"){
+    console.log("Pode passar")
+}
+else if (cor1 == "Amarelo" ){
+    console.log("Atenção")
+}
+else if (cor1 === "Vermelho"){
+    console.log("Pare")
+}
